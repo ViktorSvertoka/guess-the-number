91432e99d44cffe07ace207ac0d7d6bafe804e7d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-07 00:08:20 +0300

    Feat(JS): started writing a script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.querySelector('.guess-message');
+
+// console.log(document.querySelector('.guess-message').textContent);
+
+document.querySelector('.guess-message').textContent = 'Правильно!';
+
+// console.log(document.querySelector('.guess-message').textContent);
+
+document.querySelector('.question').textContent = 7;
+
+document.querySelector('.score').textContent = 11;
+
+document.querySelector('.number-input').value = 13;
+
+// console.log(document.querySelector('.number-input').value);
