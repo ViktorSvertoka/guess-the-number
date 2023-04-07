518fa3e6a2fbf6e1a98c66eb737621240454a562
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-08 01:32:02 +0300

    Feat(JS): added function displayGuessMassage

--- script.js
+++ script.js
@@ -25,17 +25,23 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayGuessMassage = function () {
+  document.querySelector('.question').textContent = 'message';
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(typeof guessingNumber);
 
   // No input
   if (!guessingNumber) {
-    document.querySelector('.guess-message').textContent = 'Введите число!';
+    displayGuessMassage('Введите число!');
+    // document.querySelector('.guess-message').textContent = 'Введите число!';
 
     // Player won
   } else if (guessingNumber === secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Правильно!';
+    displayGuessMassage('Правильно!');
+    // document.querySelector('.guess-message').textContent = 'Правильно!';
     document.querySelector('.question').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
     document.querySelector('.question').style.width = '50rem';
@@ -47,12 +53,16 @@ document.querySelector('.check').addEventListener('click', function () {
     // Number from input is wrong
   } else if (guessingNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.guess-message').textContent =
-        guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!';
+      displayGuessMassage(
+        guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!',
+      );
+      //   document.querySelector('.guess-message').textContent =
+      //     guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+      displayGuessMassage('Вы проиграли!');
+      //   document.querySelector('.guess-message').textContent = 'Вы проиграли!';
       document.querySelector('body').style.backgroundColor = 'rgb(250, 9, 21)';
       document.querySelector('.question').textContent = 'GAME OVER';
       document.querySelector('.score').textContent = 0;
@@ -101,7 +111,8 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.question').textContent = '???';
   document.querySelector('.question').style.width = '25rem';
-  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  displayGuessMassage('Начни угадывать!');
+  //   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number-input').value = '';
   document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
