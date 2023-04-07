01a946a87fa4be5756a4329b6c870c5de93b6d79
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-08 00:50:57 +0300

    Feat(JS): added code to high score

--- script.js
+++ script.js
@@ -24,6 +24,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
@@ -39,8 +40,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.question').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
     document.querySelector('.question').style.width = '50rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
-    // to hight
+    // to high
   } else if (guessingNumber > secretNumber) {
     if (score > 1) {
       document.querySelector('.guess-message').textContent = 'Слишком много!';
