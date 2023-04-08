8cdb3e38e6ab2853a2a88770d84faadad5f013ee
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-08 15:09:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,16 +28,13 @@ document.querySelector('.check').addEventListener('click', function () {
   // No input
   if (!guessingNumber) {
     displayGuessMassage('Введите число!');
-    // document.querySelector('.guess-message').textContent = 'Введите число!';
 
     // Player won
   } else if (guessingNumber === secretNumber) {
     displayGuessMassage('Правильно!');
-    // document.querySelector('.guess-message').textContent = 'Правильно!';
     displayQuestionMassage(secretNumber);
-    // document.querySelector('.question').textContent = secretNumber;
     backgroundColor('rgb(9, 250, 21)');
-    // document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+
     document.querySelector('.question').style.width = '50rem';
     if (score > highScore) {
       highScore = score;
@@ -50,17 +47,12 @@ document.querySelector('.check').addEventListener('click', function () {
       displayGuessMassage(
         guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!',
       );
-      //   document.querySelector('.guess-message').textContent =
-      //     guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       displayGuessMassage('Вы проиграли!');
-      //   document.querySelector('.guess-message').textContent = 'Вы проиграли!';
       backgroundColor('rgb(250, 9, 21)');
-      // document.querySelector('body').style.backgroundColor = 'rgb(250, 9, 21)';
       displayQuestionMassage('GAME OVER');
-      //   document.querySelector('.question').textContent = 'GAME OVER';
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -72,14 +64,11 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
 
   displayQuestionMassage('???');
-  //   document.querySelector('.question').textContent = '???';
   document.querySelector('.question').style.width = '25rem';
   displayGuessMassage('Начни угадывать!');
-  //   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number-input').value = '';
   backgroundColor('rgb(0, 0, 0)');
-  // document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
 });
 
 //! Вариант перезагрузки страницы № 2, но тогда не сохранится High Score
