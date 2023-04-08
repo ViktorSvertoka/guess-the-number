9b1203e0b0793ffce986e08b34e00c42f4029f6c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-08 12:57:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// document.querySelector('.guess-message');
-
-// console.log(document.querySelector('.guess-message').textContent);
-
-// document.querySelector('.guess-message').textContent = 'Правильно!';
-
-// console.log(document.querySelector('.guess-message').textContent);
-
-// document.querySelector('.question').textContent = 7;
-
-// document.querySelector('.score').textContent = 11;
-
-// document.querySelector('.number-input').value = 13;
-
-// console.log(document.querySelector('.number-input').value);
-
 //? Вариант - передача функции в значении переменной.
 // const eventHandler = function () {
 //   console.log(document.querySelector('.number-input').value);
